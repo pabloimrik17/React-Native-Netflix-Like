44b249ad5e9146c422bf39abd1b560a5bb57903a
Etherless <pabloimrik17@gmail.com>
2018-01-14 02:29:53 +0100

    New Header Component. New Menu Component

--- App.js
+++ App.js
@@ -10,17 +10,47 @@ import {
     View
 } from 'react-native';
 
+import SideMenu from 'react-native-side-menu';
 import List from './src/components/List';
 import Slider from './src/components/Slider';
+import Header from './src/components/Header';
+import Menu from './src/components/Menu';
 
 export default class App extends Component<{}> {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isOpen: false,
+        };
+    }
+
     render() {
         return (
-            <View style={[styles.container, {flex: 1}]}>
-                <Slider/>
-                <List/>
+            <View style={{flex: 1}}>
+                <SideMenu
+                    style={{backgroundColor:'#000000'}}
+                    menu={<Menu/>}
+                    isOpen={this.state.isOpen}
+                    onChange={(isOpen) => this.updateMenu(isOpen)}
+                >
+                    <View style={[styles.container, {flex: 1}]}>
+                        <Header toggle={this.toggle.bind(this)}/>
+                        <Slider/>
+                        <List/>
+                    </View>
+                </SideMenu>
             </View>
         );
+    }
+
+    toggle() {
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    }
+
+    updateMenu(isOpen) {
+        this.setState({isOpen});
     }
 }
 
